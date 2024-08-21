0a3f13a5e47e4c32c42d0a7751dd86c74b9b6adb
rwalia <rohanwalia9000@gmail.com>
2024-08-21 19:12:44 +0200

    add wandb to rl

--- policy_learning/sb3v3.py
+++ policy_learning/sb3v3.py
@@ -1,7 +1,6 @@
 ## Taken from https://github.com/johnnycode8/gym_solutions/blob/main/sb3v3.py
 ## Modified by: Billot, Garzón, Walia
 
-
 from myosuite.utils import gym
 import stable_baselines3
 from stable_baselines3 import SAC
@@ -9,15 +8,26 @@ from stable_baselines3.common.callbacks import StopTrainingOnNoModelImprovement,
 from stable_baselines3.common.monitor import Monitor
 import os
 import argparse
+import wandb
 
-# Create directories to hold models and logs
-model_dir = "models"
-log_dir = "logs"
-os.makedirs(model_dir, exist_ok=True)
-os.makedirs(log_dir, exist_ok=True)
 
-def train():
-    model = sb3_class('MlpPolicy', env, verbose=1, device='cuda', tensorboard_log=log_dir)
+def train(run_name):
+    # Initialize wandb with TensorBoard sync
+    wandb.init(
+        project="myoback",
+        config={
+            "environment": args.gymenv,
+            "algorithm": args.sb3_algo
+        },
+        sync_tensorboard=True,  # Synchronize TensorBoard with wandb
+        name=run_name
+    )
+    
+    # Define the tensorboard log directory
+    tensorboard_log_dir = f"{os.environ.get('project')}/MyoBack/policy_learning/logs/{run_name}"
+    model_log_dir = f"{os.environ.get('project')}/MyoBack/policy_learning/models/{run_name}"
+    
+    model = sb3_class('MlpPolicy', env, verbose=1, device='cuda', tensorboard_log=tensorboard_log_dir)
 
     # Stop training when mean reward reaches reward_threshold
     callback_on_best = StopTrainingOnRewardThreshold(reward_threshold=300, verbose=1)
@@ -28,25 +38,26 @@ def train():
     stop_train_callback = StopTrainingOnNoModelImprovement(max_no_improvement_evals=5, min_evals=10000, verbose=1)
 
     eval_callback = EvalCallback(
-        env, 
-        eval_freq=10000, # how often to perform evaluation i.e. every 10000 timesteps.
-        callback_on_new_best=callback_on_best, 
-        callback_after_eval=stop_train_callback, 
-        verbose=1, 
-        best_model_save_path=os.path.join(model_dir, f"{args.gymenv}_{args.sb3_algo}"),
+        env,
+        eval_freq=10000,  # how often to perform evaluation i.e. every 10000 timesteps.
+        callback_on_new_best=callback_on_best,
+        callback_after_eval=stop_train_callback,
+        verbose=1,
+        best_model_save_path=os.path.join(model_log_dir, f"{run_name}_{args.gymenv}_{args.sb3_algo}_best"),
     )
-    
+
     """
     total_timesteps: pass in a very large number to train (almost) indefinitely.
     tb_log_name: create log files with the name [gym env name]_[sb3 algorithm] i.e. Pendulum_v1_SAC
-    callback: pass in reference to a callback fuction above
+    callback: pass in reference to a callback function above
     """
-    model.learn(total_timesteps=int(1e10), tb_log_name=f"{args.gymenv}_{args.sb3_algo}", callback=eval_callback)
+    model.learn(total_timesteps=int(1e10), callback=eval_callback, tb_log_name=f"{args.gymenv}_{args.sb3_algo}")
 
-def test():        
-    model = sb3_class.load(os.path.join(model_dir, f"{args.gymenv}_{args.sb3_algo}", "best_model"), env=env)
+def test(run_name):
+    model_log_dir = f"{os.environ.get('project')}/MyoBack/policy_learning/models/{run_name}"
+    model = sb3_class.load(os.path.join(model_log_dir, f"{run_name}_{args.gymenv}_{args.sb3_algo}_best"), env=env)
 
-    obs = env.reset()[0]   
+    obs = env.reset()[0]
     while True:
         action, _ = model.predict(obs)
         obs, _, terminated, truncated, _ = env.step(action)
@@ -54,14 +65,13 @@ def test():
         if terminated or truncated:
             break
 
-
 if __name__ == '__main__':
-
     # Parse command line inputs
     parser = argparse.ArgumentParser(description='Train or test model.')
     parser.add_argument('gymenv', help='Gymnasium environment i.e. Humanoid-v4')
-    parser.add_argument('sb3_algo', help='StableBaseline3 RL algorithm i.e. A2C, DDPG, DQN, PPO, SAC, TD3')    
+    parser.add_argument('sb3_algo', help='StableBaseline3 RL algorithm i.e. A2C, DDPG, DQN, PPO, SAC, TD3')
     parser.add_argument('--test', help='Test mode', action='store_true')
+    parser.add_argument('--run_name', help='WandB run name', required=True)
     args = parser.parse_args()
 
     # Dynamic way to import algorithm. For example, passing in DQN is equivalent to hardcoding:
@@ -70,10 +80,11 @@ if __name__ == '__main__':
 
     if args.test:
         env = gym.make(args.gymenv, render_mode='human')
-        test()
+        test(args.run_name)
     else:
         env = gym.make(args.gymenv)
         env = Monitor(env)
-        # env = gym.wrappers.RecordVideo(env, video_folder=recording_dir, episode_trigger = lambda x: x % 10000 == 0)
-        train()
-        
+        # env = gym.wrappers.RecordVideo(env, video_folder=recording_dir, episode_trigger=lambda x: x % 10000 == 0)
+        train(args.run_name)
+
+    wandb.finish()

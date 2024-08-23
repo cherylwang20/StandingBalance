8131e1dd326c070fd6f5912672bb5507a4558027
rwalia <rohanwalia9000@gmail.com>
2024-08-23 22:51:03 +0200

    remove entity

--- policy_learning/sb3v3.py
+++ policy_learning/sb3v3.py
@@ -15,7 +15,6 @@ def train(run_name):
     # Initialize wandb with TensorBoard sync
     wandb.init(
         project="myoback",
-        entity="rwalia",
         config={
             "environment": args.gymenv,
             "algorithm": args.sb3_algo
